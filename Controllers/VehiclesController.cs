4ec498f601ff022f4ffe65534c2481667c3a14f1
Adebayo Kehinde <press.spark@gmail.com>
2018-07-13 17:33:19 +0200

    Latest Changes to MappingProfile

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -22,7 +22,7 @@ namespace playground.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> CreateVehicle([FromBody] VehicleResource vehicleResource)
+        public async Task<IActionResult> CreateVehicle([FromBody] SaveVehicleResource vehicleResource)
         {
             if(!ModelState.IsValid)
               return BadRequest(ModelState);
@@ -34,16 +34,16 @@ namespace playground.Controllers
             //     return BadRequest(ModelState);
             // }
 
-            var vehicle = Mapper.Map<VehicleResource, Vehicle>(vehicleResource);
+            var vehicle = Mapper.Map<SaveVehicleResource, Vehicle>(vehicleResource);
             vehicle.LastUpdate = DateTime.Now;
             context.Vehicles.Add(vehicle);
             await context.SaveChangesAsync();
 
-            var result = mapper.Map<Vehicle,VehicleResource>(vehicle);
+            var result = mapper.Map<Vehicle,SaveVehicleResource>(vehicle);
             return Ok(result);
         }
         [HttpPut("{id}")] // /api/vehicles/{id}
-        public async Task<IActionResult> UpdateVehicle(int id, [FromBody] VehicleResource vehicleResource)
+        public async Task<IActionResult> UpdateVehicle(int id, [FromBody] SaveVehicleResource vehicleResource)
         {
             if(!ModelState.IsValid)
               return BadRequest(ModelState);
@@ -51,12 +51,15 @@ namespace playground.Controllers
             var vehicle = await context.Vehicles.Include(v=>v.Features)
             .SingleOrDefaultAsync(v=>v.Id == id);
 
-            mapper.Map<VehicleResource, Vehicle>(vehicleResource, vehicle);
+            if(vehicle == null)
+              return NotFound();
+
+            mapper.Map<SaveVehicleResource, Vehicle>(vehicleResource, vehicle);
             vehicle.LastUpdate = DateTime.Now;
             
             await context.SaveChangesAsync();
 
-            var result = mapper.Map<Vehicle,VehicleResource>(vehicle);
+            var result = mapper.Map<Vehicle, SaveVehicleResource>(vehicle);
             return Ok(result);
         }
 
@@ -66,12 +69,27 @@ namespace playground.Controllers
             var vehicle = await context.Vehicles.FindAsync(id);
 
             if(vehicle == null)
-              NotFound();
+              return NotFound();
 
             context.Remove(vehicle);
             await context.SaveChangesAsync();
 
             return Ok(id);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetVehicle(int id)
+        {
+            var vehicle = await context.Vehicles
+                             .Include(v => v.Features)
+                             .ThenInclude(vf => vf.Feature)
+                             .SingleOrDefaultAsync(v => v.Id == id);
+
+            if(vehicle == null)
+              return NotFound();
+
+           var vehicleResource = mapper.Map<Vehicle, VehicleResource>(vehicle);
+
+            return Ok(vehicleResource);
         }
     }
 }
--- Mapping/MappingProfile.cs
+++ Mapping/MappingProfile.cs
@@ -14,43 +14,49 @@ namespace playground.Mapping
             CreateMap<Make, MakeResource>();
             CreateMap<Model, ModelResource>();
             CreateMap<Feature, FeatureResource>();
+            CreateMap<Vehicle, SaveVehicleResource>()
+            .ForMember(vr => vr.Contact, opt => opt.MapFrom(v => new ContactResource { Name = v.ContactName, Email =v.ContactEmail,Phone= v.ContactPhone}))
+            .ForMember(vr => vr.Features, opt => opt.MapFrom(v=> v.Features.Select(vf=>vf.FeatureId)));
             CreateMap<Vehicle, VehicleResource>()
-            .ForMember(vr => vr.Contact, opt => opt.MapFrom(v => 
-            new ContactResource{ Name = v.ContactName, Email =v.ContactEmail,Phone= v.ContactPhone}))
-            .ForMember(vr=>vr.Features, opt =>opt.MapFrom(v=> v.Features.Select(vf=>vf.FeatureId)));
+            .ForMember(vr => vr.Contact, opt => opt.MapFrom(v => new ContactResource{ Name = v.ContactName, Email =v.ContactEmail,Phone= v.ContactPhone}))
+            .ForMember(vr => vr.Features, opt =>opt.MapFrom(v=> v.Features.Select(vf=> new FeatureResource {Id = vf.Feature.Id, Name = vf.Feature.Name})));
+      
             //These maps are unidirectional just Make to MakeResource  
 
             //API Resource to Domain
-            CreateMap<VehicleResource, Vehicle>()
+            CreateMap<SaveVehicleResource, Vehicle>()
             .ForMember(v => v.Id, opt => opt.Ignore())
             .ForMember(v => v.ContactName, opt => opt.MapFrom(vr => vr.Contact.Name))         
             .ForMember(v => v.ContactEmail, opt => opt.MapFrom(vr => vr.Contact.Email))         
             .ForMember(v => v.ContactPhone, opt => opt.MapFrom(vr => vr.Contact.Phone))
-            .ForMember(v=>v.Features, opt=>opt.Ignore())
+            .ForMember(v => v.Features, opt=>opt.Ignore())
             .AfterMap((vr, v)=> {
-                //Remove unselected features
-                // var removedFeatures = new List<VehicleFeature>();
-                // foreach(var f in v.Features) {
-                //     if(!vr.Features.Contains(f.FeatureId))
-                //        removedFeatures.Add(f);
-                var removedFeatures = v.Features.Where(f=> !vr.Features.Contains(f.FeatureId));
+               // Remove unselected features
+                var removedFeatures = new List<VehicleFeature>();
+                foreach(var f in v.Features) 
+                    if(!vr.Features.Contains(f.FeatureId))
+                       removedFeatures.Add(f);
+
+                // var removedFeatures = v.Features.Where(f=> !vr.Features.Contains(f.FeatureId));
                
-                foreach(var f in removedFeatures){
-                    v.Features.Remove(f);
+                foreach(var id in removedFeatures){
+                    v.Features.Remove(id);
                 }
 
                 //Add new features
-                // foreach (var id in vr.Features)
-                // {
-                //     if(!v.Features.Any(f=>f.FeatureId == id))
-                //     v.Features.Add(new VehicleFeature{ FeatureId = id});
+                foreach (var id in vr.Features)
+                {
+                    if(!v.Features.Any(f => f.FeatureId == id))
+                    v.Features.Add(new VehicleFeature{ FeatureId = id });
                     
-                // }
-                var addedFeatures = vr.Features.Where(id => !v.Features.Any(f=>f.FeatureId == id))
-                .Select(id => new VehicleFeature {FeatureId = id});
-                foreach (var f in addedFeatures)
-                  v.Features.Add(f);
-            });       
+                }
+
+                //LINQ REFACTORING
+                // var addedFeatures = vr.Features.Where(id => !v.Features.Any(f=>f.FeatureId == id))
+                // .Select(id => new VehicleFeature {FeatureId = id});
+                // foreach (var f in addedFeatures)
+                //   v.Features.Add(f);
+                });       
             // .ForMember(v => v.Features, opt => 
             // opt.MapFrom(vr => vr.Features.Select(id=> new VehicleFeature {FeatureId = id})));        
         }
