1f4816136137f523256d4e7321f8500dc14ec8a4
Adebayo Kehinde <press.spark@gmail.com>
2018-07-18 03:44:40 +0200

    Implements delete logics

--- ClientApp/src/app/vehicle-form/vehicle-form.component.ts
+++ ClientApp/src/app/vehicle-form/vehicle-form.component.ts
@@ -111,4 +111,11 @@ export class VehicleFormComponent implements OnInit {
     }
 
   }
+  delete(){
+    if(confirm('Are you sure?'))
+    this.vehicleService.delete(this.vehicle.id)
+    .subscribe(x=>{
+      this.router.navigate(['/']);
+    })
+  }
 }
