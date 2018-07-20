f106d0d81b3ad110dbc2b9324ad488f452c9cc43
Adebayo Kehinde <press.spark@gmail.com>
2018-07-20 16:00:06 +0200

    Sending of client sort parameters to server

--- ClientApp/src/app/vehicle-list/vehicle-list.component.ts
+++ ClientApp/src/app/vehicle-list/vehicle-list.component.ts
@@ -10,7 +10,7 @@ import { Component, OnInit } from '@angular/core';
 export class VehicleListComponent implements OnInit {
   vehicles: Vehicle[];
   makes: KeyValuePair[];
-  filter: any = {};
+  query: any = {};
 
   constructor(private vehicleService: VehicleService) { }
 
@@ -22,17 +22,27 @@ export class VehicleListComponent implements OnInit {
   }
   
   private populateVehicles(){
-    this.vehicleService.getVehicles(this.filter)
+    this.vehicleService.getVehicles(this.query)
       .subscribe(vehicles => this.vehicles = <Vehicle[]>vehicles);
   }
 
   onFilterChange(){
-    this.filter.modelId = 5;
+   // this.filter.modelId = 5;
     this.populateVehicles();
   }
 
   resetFilter(){
-    this.filter = {};
+    this.query = {};
     this.onFilterChange();
+  }
+
+  sortBy(columnName){
+    if(this.query.sortBy = columnName){
+      this.query.isSortAscending = false;
+    } else{
+      this.query.sortBy = columnName;
+      this.query.isSortAscending = true;
+    }
+    this.populateVehicles();
   }
 }
