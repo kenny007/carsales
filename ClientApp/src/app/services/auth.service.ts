63adb16d9d32b9c4e0334e00019760d13db363a8
Adebayo Kehinde <press.spark@gmail.com>
2018-08-05 18:52:39 +0100

    Getting the users profile

--- ClientApp/src/app/services/auth.service.ts
+++ ClientApp/src/app/services/auth.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { filter } from 'rxjs/operators';
 import * as auth0 from 'auth0-js';
-import  Auth0Lock  from 'auth0-lock';
 
 (window as any).global = window;
 
@@ -11,36 +10,37 @@ import  Auth0Lock  from 'auth0-lock';
 export class AuthService {
   profile: any;
 
+
   auth0 = new auth0.WebAuth({
     clientID: 's0EU4jh5aKV2RiV8p2mCxV39bgFA6dTQ',
     domain: 'carsale.auth0.com',
     responseType: 'token id_token',
     audience: 'https://carsale.auth0.com/userinfo',
     redirectUri: 'http://localhost:3000/callback',
-    scope: 'openid'
+    scope: 'openid profile'
   });
-
-  lock = new Auth0Lock(
-    's0EU4jh5aKV2RiV8p2mCxV39bgFA6dTQ',
-    'carsale.auth0.com'
-  );
-  
 
   constructor(public router: Router) {
     this.profile = JSON.parse(localStorage.getItem('profile'));
-     debugger;
+    const accessToken = localStorage.getItem('access_token');
     
-     this.lock.on("authenticated", (authResult) => {
-      localStorage.setItem('token', authResult.accessToken);
-   
-      this.lock.getUserInfo(authResult.accessToken, (error, profile) => {
-        if(error)
-        throw error;
+    if(accessToken){
+      const self = this;
 
-        localStorage.setItem('profile', JSON.stringify(profile))
-        this.profile = profile;
-      })
-    })
+      if(!self.profile){
+      this.auth0.client.userInfo(accessToken, (err, profile) => { 
+      debugger;
+      
+      localStorage.setItem('profile', JSON.stringify(profile))
+      self.profile = profile;
+      //cb(err, profile);
+      (err, profile) => {
+        if (err) throw err
+        }
+       });
+      }
+    }
+
   }
 
   public login(): void {
@@ -69,12 +69,16 @@ export class AuthService {
         localStorage.setItem('access_token', authResult.accessToken);
         localStorage.setItem('id_token', authResult.idToken);
         localStorage.setItem('expires_at', expiresAt);
+        localStorage.setItem('profile', JSON.stringify(authResult.idTokenPayload));
+        this.profile = null;
+        //console.log(authResult);
       }
     
       public logout(): void {
         // Remove tokens and expiry time from localStorage
         localStorage.removeItem('access_token');
         localStorage.removeItem('id_token');
+        localStorage.removeItem('profile');
         localStorage.removeItem('expires_at');
         // Go back to the home route
         this.router.navigate(['/']);
